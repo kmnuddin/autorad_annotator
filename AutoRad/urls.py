58e4587c048b66aa1041a9c65409aae18d79d9bf
kmnuddin <kmnuddin@memphis.edu>
2024-01-14 01:09:57 -0600

    Need to implement model api

--- AutoRad/urls.py
+++ AutoRad/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from AutoRad.views import home, upload_image
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('upload-path/', upload_image, name='upload_image')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- AutoRad/views.py
+++ AutoRad/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+import torch
 def home(request):
     return render(request, 'home.html')
 
@@ -13,3 +14,22 @@ def upload_image(request):
         context['image_url'] = image_url
 
     return render(request, 'home.html', context)
+
+@api_view(['POST'])
+def process_image(request):
+    if request.method == 'POST' and request.FILES['image']:
+        # Save image
+        image = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(image.name, image)
+        image_path = fs.path(filename)
+
+        # Process image and predict mask with U-Net model
+        mask = your_unet_model.predict(image_path)  # Adjust this line to use your model
+
+        # Save mask image and get URL
+        mask_filename = fs.save('mask_' + filename, mask)
+        mask_url = fs.url(mask_filename)
+
+        return Response({'mask_url': mask_url})
+    return Response({'error': 'No image provided'}, status=400)
