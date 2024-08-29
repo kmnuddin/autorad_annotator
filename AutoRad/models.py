e7f1f849994b9c63c22dbb1567c6e97e76cd5502
UMLijiaWang <wlj54188@gmail.com>
2024-08-29 16:49:49 -0500

    Create new class v 0.1
    Create Image, mask, and report class

--- AutoRad/models.py
+++ AutoRad/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
-class img(models.Model):
+class imgClass(models.Model):
     imgName = models.CharField(max_length=200)
+    imgFile = models.ImageField(upload_to='.\media')
+    docID = models.IntegerField()
+    reportID = models.IntegerField()
+    
+class maskClass(models.Model):
+    maskName = models.CharField(max_length=200)
+    maskType = models.CharField(max_length=200)
+    imageID = models.IntegerField()
+    maskFile = models.CharField(max_length=200)
+    maskProperty = models.CharField(max_length=200)
+    
+class reprotClass(models.Model):
+    reportName = models.CharField(max_length=200)
+    imgId = models.IntegerField()
+    reportContent = models.CharField(max_length=200)
+    
