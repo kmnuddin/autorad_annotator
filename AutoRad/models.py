4b0298871ebe0f07d9eea8366cdec4244371cbc5
UMLijiaWang <wlj54188@gmail.com>
2024-09-11 01:35:41 -0500

    DB creation and testing
    1. Create DB in sqlite3, including patients, img, masks, reports.
    2. Add some testing features on the home page.

--- AutoRad/models.py
+++ AutoRad/models.py
@@ -1,20 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+### Each user will have multiple imgs:   user --[1 to many] --> img
+### Each image will have multiple masks: img --[1 to many] --> mask
+### Each image will have only one report: img --[1 to 1] --> img
+
+class patientClass(models.Model):
+    patientID = models.CharField(max_length=100,default="000000000000")
+    patientName = models.CharField(max_length=100,default="")
+    
+    userID = models.OneToOneField(User,on_delete=models.CASCADE,default="000000000000")
+    
+class reportClass(models.Model):
+    reportName = models.CharField(max_length=200,default="")
+    reprotID = models.CharField(max_length=100,default="000000000000")
+    reportContent = models.CharField(max_length=200,default="")
+    
+    patientID = models.ForeignKey(patientClass,on_delete=models.CASCADE,default="000000000000")
+    
 class imgClass(models.Model):
-    imgName = models.CharField(max_length=200)
-    imgFile = models.ImageField(upload_to='.\media')
-    docID = models.IntegerField()
-    reportID = models.IntegerField()
+    imgKey = models.CharField(max_length=100,help_text="The public ID of the upoloaded file.",default="000000000000")
+    imgName = models.CharField(max_length=100,help_text="The name of the uploaded image.",default="")
+    url = models.CharField(max_length=100,default='.\media')
+    imgFile = models.ImageField(upload_to='media')
+    width = models.IntegerField(help_text="Width in px",default=320)
+    height = models.IntegerField(help_text="height in px",default=320)
+    format = models.CharField(max_length=10,default="jpg")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)    
+    
+    # patientID = models.ForeignKey(patientClass,on_delete=models.CASCADE)
+    reportID = models.ForeignKey(reportClass,on_delete=models.CASCADE,default="000000000000")
     
 class maskClass(models.Model):
-    maskName = models.CharField(max_length=200)
-    maskType = models.CharField(max_length=200)
-    imageID = models.IntegerField()
-    maskFile = models.CharField(max_length=200)
-    maskProperty = models.CharField(max_length=200)
+    maskKey = models.CharField(max_length=100,help_text="The internal ID of the mask.",default="000000000000")
+    maskName = models.CharField(max_length=200,help_text="The name of the mask",default="")
+    maskType = models.CharField(max_length=200,help_text="The type of the mask, ",default="")
+    # maskFile = models.CharField(max_length=200)
+    maskPts = models.CharField(max_length=1000,default="[]")
+    maskTop = models.IntegerField(default=0)
+    maskLeft = models.IntegerField(default=0)
+    maskAngle = models.IntegerField(default=0)
+    maskScale = models.FloatField(default=1.0)
+    maskOpacity = models.FloatField(default=1.0)
+    maskCornerColor = models.CharField(max_length = 7, help_text = "The color code for corner",default="#0000ff")
+    maskStrokeColor = models.CharField(max_length = 7, help_text = "The color code for line",default="#ff0000")
     
-class reprotClass(models.Model):
-    reportName = models.CharField(max_length=200)
-    imgId = models.IntegerField()
-    reportContent = models.CharField(max_length=200)
+    imgID = models.ForeignKey(imgClass,on_delete=models.CASCADE,default="000000000000")
+    
+
     
