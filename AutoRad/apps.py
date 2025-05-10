9978107ec1a6f2f44e3c74cbb274a59489f9cb93
kmnuddin <kmnuddin@memphis.edu>
2025-05-10 13:44:41 -0500

    Edited Apps Ready and deleted staticfiles

--- AutoRad/apps.py
+++ AutoRad/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
 from .utils import load_model
+import sys
 class AutoRadConfig(AppConfig):
     name = 'AutoRad'
 
     def ready(self):
         import AutoRad.signals
+
+        if any(cmd in sys.argv for cmd in ('collectstatic', 'migrate', 'makemigrations', 'test')):
+            return
         load_model()
 
