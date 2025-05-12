a750f4698a43ca2f69e63624b45b133267a4ec27
kmnuddin <kmnuddin@memphis.edu>
2025-05-12 13:07:58 -0500

    Configured whitenoise for media storage.

--- AutoRad/wsgi.py
+++ AutoRad/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutoRad.settings')
-
 application = get_wsgi_application()
+
+# first serve your STATIC_ROOT (already done)
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+# then also serve your MEDIA_ROOT under /media/
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
+
